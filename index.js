7b0de12056a8f5f1795d9028041037461287f3e3
surunipo <jesuyon324@gmail.com>
2025-05-11 16:56:20 +0100

    BMI Group_2 Assignment update

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+	res.send('Welcome to our express.js server');
+});
+app.get('/home', (req, res) => {
+	res.send('Welcome home');
+});
+app.get('/friend', (req, res) => {
+	res.send('Hello friend');
+});
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
