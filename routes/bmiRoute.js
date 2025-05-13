552ca63e0505f41450a1759cab9c91e748f2dc39
Mohammed <95849758+oyediranmohammed@users.noreply.github.com>
2025-05-14 02:28:11 +0400

    updated routes

--- routes/bmiRoute.js
+++ routes/bmiRoute.js
@@ -1,11 +1,33 @@
 import express from 'express';
+import validateBMI from '../middleware/validateBMI.js'
+import calBMI from "../controllers/bmi-function.js"
 
 const router = express.Router();
 
-// Placeholder routes
-router.get('/', (req, res) => {
-  res.json({ message: 'Hi' });
-});
+// GET request to calculate BMI
+const getBmi = (req, res) => {
+  const { unit, weight, height } = req.query;
+  const result = calBMI(unit, parseFloat(weight), parseFloat(height));
+  if (result.error) {
+    return res.status(400).json({ error: result.error });
+  }
 
+  res.json(result);
+};
+
+// POST request to calculate BMI
+const postBmi = (req, res) => {
+  const { unit, weight, height } = req.body;
+  const result = calBMI(unit, parseFloat(weight), parseFloat(height));
+  if (result.error) {
+    return res.status(400).json({ error: result.error });
+  }
+
+  res.json(result);
+};
+
+
+router.get('/bmi', validateBMI, getBmi); // GET request to calculate BMI
+router.post('/bmi', validateBMI, postBmi); // POST request to calculate BMI
 
 export default router;
