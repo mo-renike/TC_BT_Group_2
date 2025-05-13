b6714b79b051b3cd4bb037675c70d7c080fffe52
Mohammed <95849758+oyediranmohammed@users.noreply.github.com>
2025-05-14 01:39:24 +0400

    corrected spelling errors

--- middleware/validateBMI.js
+++ middleware/validateBMI.js
@@ -6,14 +6,14 @@ function validateBMI (req, res, next) {
         return res.status(400).json({ error: "Weight and Height values are required." })
     }
 
-    // This checks if the inputted values are numbers
+    // This checks if the inputed values are numbers
     if (typeof weight !== "number" || typeof  height !== "number") {
-        return res.status(400).json({ error: "Weight and height inputted values must be number." })
+        return res.status(400).json({ error: "Weight and height inputed values must be number." })
     }
 
-    // This checks if the inputted values are positive 
+    // This checks if the inputed values are positive 
     if (weight <= 0 || height <=0) {
-        return res.status(400).json({ error: "Weight and height inputted values must be positive." })
+        return res.status(400).json({ error: "Weight and height inputed values must be positive." })
     }
 
     //When all checks are passed it moves to next
