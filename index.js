2902f7fd8187fbbdb49f230dbe9a46f5a54359b9
Mohammed <95849758+oyediranmohammed@users.noreply.github.com>
2025-05-14 02:16:17 +0400

    Added POST request with some comments

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import bmiRoutes from './routes/bmi-route.js';
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 app.use('/', bmiRoutes);
 
 app.listen(PORT, () => {
--- middleware/validateBMI.js
+++ middleware/validateBMI.js
@@ -1,28 +1,42 @@
 function validateBMI(req, res, next) {
-    const source = req.method === 'GET' ? req.query : req.body;
-    const { unit, weight, height } = source;
+  const source = req.method === "GET" ? req.query : req.body;
 
-    // This checks if both fields are present 
-    if (!unit || !weight || !height) {
-        return res.status(400).json({ error: "Weight and Height values are required." })
-    }
+  // This checks if the request has a body or query
+  if (!source) {
+    return res.status(400).json({ error: "Missing input data" });
+  }
 
-    // This checks if the inputted values are numbers
-    // if (typeof weight !== "number" || typeof  height !== "number") {
-    if (isNaN(weight) || isNaN(height)) {
-        return res.status(400).json({ error: "Weight and height inputted values must be number." })
-    }
+  const { unit, weight, height } = source;
 
-    // This checks if the inputted values are positive 
-    if (parseFloat(weight) <= 0 || parseFloat(height) <= 0) {
-        return res.status(400).json({ error: "Weight and height inputted values must be positive." })
-    }
+  // This checks if both fields are present
+  if (!unit || !weight || !height) {
+    return res
+      .status(400)
+      .json({ error: "Weight and Height values are required." });
+  }
 
-    if (unit !== 'metric' && unit !== 'imperial') {
-        return res.status(400).json({ error: "Unit must be either 'metric' or 'imperial'." });
-    }
-    //When all checks are passed it moves to next
-    next();
+  // This checks if the inputted values are numbers
+  // if (typeof weight !== "number" || typeof  height !== "number") {
+  if (isNaN(weight) || isNaN(height)) {
+    return res
+      .status(400)
+      .json({ error: "Weight and height inputted values must be number." });
+  }
+
+  // This checks if the inputted values are positive
+  if (parseFloat(weight) <= 0 || parseFloat(height) <= 0) {
+    return res
+      .status(400)
+      .json({ error: "Weight and height inputted values must be positive." });
+  }
+
+  if (unit !== "metric" && unit !== "imperial") {
+    return res
+      .status(400)
+      .json({ error: "Unit must be either 'metric' or 'imperial'." });
+  }
+  //When all checks are passed it moves to next
+  next();
 }
 
 export default validateBMI;
