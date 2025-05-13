1c362482eb3d49efc4a6e3d0aef4df7fca0511c0
Mohammed <95849758+oyediranmohammed@users.noreply.github.com>
2025-05-14 02:29:44 +0400

    update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-import calBMI from './controllers/bmi-function.js';
+
 import express from 'express';
-import bmiRoutes from './routes/bmiRoute.js';
+import bmiRoutes from './routes/bmi-route.js';
 
 const app = express();
 const PORT = 3000;
 
-app.use('/api/bmi', bmiRoutes);
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use('/', bmiRoutes);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
